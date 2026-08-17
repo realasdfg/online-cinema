eadfa7e9d37a34e1aa07602b830ff6757cebe788
Roman Popadiuk <realasdfga@gmail.com>
2026-08-18 01:43:45 +0200

    Implement User model

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Enum, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, Enum, ForeignKey, Integer, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.models.enums import UserGroupEnum
 from database.session import Base
@@ -13,5 +15,33 @@ class UserGroup(Base):
         Enum(UserGroupEnum), nullable=False, unique=True
     )
 
+    users: Mapped[list["User"]] = relationship("User", back_populates="group")
+
     def __repr__(self):
         return f"<UserGroup(id={self.id}, name={self.name})>"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    group_id: Mapped[int] = mapped_column(
+        ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False
+    )
+
+    group: Mapped["UserGroup"] = relationship("UserGroup", back_populates="users")
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email}, is_active={self.is_active})>"

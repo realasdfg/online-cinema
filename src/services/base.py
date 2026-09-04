ff4fa353249b9d5c5c8f8c7d1b452c054cd13d7d
Roman Popadiuk <realasdfga@gmail.com>
2026-09-04 23:51:39 +0200

    Add generics to BaseCRUDService

--- src/services/base.py
+++ src/services/base.py
@@ -1,32 +1,35 @@
+from collections.abc import Sequence
+from typing import Generic
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.repositories.repository import SQLAlchemyRepository, TModel
 
 
-class BaseCRUDService:
+class BaseCRUDService(Generic[TModel]):
     def __init__(
         self, session: AsyncSession, repo_class: type[SQLAlchemyRepository[TModel]]
     ):
         self._session = session
         self._repo = repo_class(session)
 
-    async def create(self, data: dict):
+    async def create(self, data: dict) -> TModel:
         obj = await self._repo.create(data)
         await self._session.commit()
         return obj
 
-    async def get_one_by_id(self, id_: int):
+    async def get_one_by_id(self, id_: int) -> TModel | None:
         return await self._repo.get(self._repo.model.id == id_)
 
-    async def get_all(self, *conditions, order_by=None):
+    async def get_all(self, *conditions, order_by=None) -> Sequence[TModel]:
         return await self._repo.list_all(*conditions, order_by=order_by)
 
-    async def update_by_id(self, id_: int, update_data: dict):
+    async def update_by_id(self, id_: int, update_data: dict) -> TModel | None:
         obj = await self._repo.update(self._repo.model.id == id_, data=update_data)
         await self._session.commit()
         return obj
 
-    async def delete_by_id(self, id_: int):
+    async def delete_by_id(self, id_: int) -> bool:
         result = await self._repo.delete(self._repo.model.id == id_)
         await self._session.commit()
         return result
